e6fa79c45579aa77ff87058b6ef333d095a96854
karima-hussein <36797485+karima-hussein@users.noreply.github.com>
2020-09-11 22:03:47 +0200

    Create onlineData.js

---
+++ onlineData.js
@@ -0,0 +1,84 @@
+[
+  {
+    "userName": "karima hussein",
+    "password": "123",
+    "lists": [
+      {
+        "listName": "karima's list",
+        "books": [
+          {
+            "book name": "six of crows",
+            "author": "jane austin",
+            "release date": "2020-12-31",
+            "cover image": "5f5011ba094c65.36620376.jpg"
+          },
+          {
+            "book name": "crooked kingdom",
+            "author": "leigh bardogu",
+            "release date": "2020-12-31",
+            "cover image": "5f501206938444.90231671.jpg"
+          }
+        ]
+      },
+      {
+        "listName": "read2020",
+        "books": [
+          {
+            "book name": "the rage of dragons",
+            "author": "Evan Winters",
+            "release date": "2017-07-16",
+            "cover image": "rage.jpg"
+          }
+        ]
+      },
+      {
+        "listName": "favourites",
+        "books": []
+      },
+      {
+        "listName": "test import",
+        "books": [
+          {
+            "book name": "the poppy war",
+            "author": "R.F. Kuang",
+            "release date": "2018-05-01",
+            "cover image": "35068705.jpg"
+          },
+          {
+            "book name": "the final empire",
+            "author": "Brandon Sanderson",
+            "release date": "2006-07-25",
+            "cover image": "mistborn.jpg"
+          },
+          {
+            "book name": "the rage of dragons",
+            "author": "Evan Winters",
+            "release date": "2017-07-16",
+            "cover image": "rage.jpg"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "userName": "john doe",
+    "password": "123",
+    "lists": [
+      {
+        "listName": "test list 2",
+        "books": [
+          {
+            "book name": "bbb",
+            "author": "jane austin",
+            "release date": "2020-12-31",
+            "cover image": "5f50168f5f7778.30864467.jpg"
+          }
+        ]
+      },
+      {
+        "listName": "my books",
+        "books": []
+      }
+    ]
+  }
+]
