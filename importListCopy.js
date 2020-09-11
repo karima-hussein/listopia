ca57bb8529c66dc83e69f87db9771370fbcc99fe
karima-hussein <36797485+karima-hussein@users.noreply.github.com>
2020-09-11 22:04:29 +0200

    Create importListCopy.js

---
+++ importListCopy.js
@@ -0,0 +1,23 @@
+{
+  "listName": "fantasy",
+  "books": [
+    {
+      "book name": "the poppy war",
+      "author": "R.F. Kuang",
+      "release date": "2018-05-01",
+      "cover image": "35068705.jpg"
+    },
+    {
+      "book name": "the final empire",
+      "author": "Brandon Sanderson",
+      "release date": "2006-07-25",
+      "cover image": "mistborn.jpg"
+    },
+    {
+      "book name": "the rage of dragons",
+      "author": "Evan Winters",
+      "release date": "2017-07-16",
+      "cover image": "rage.jpg"
+    }
+  ]
+}
